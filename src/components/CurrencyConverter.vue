9639846d1d212d22ba22a5591ee28e72e99a9711
Jon William Taylor <jwtaylor659@gmail.com>
2023-12-21 11:53:51 -0600

    Update

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -1,37 +1,48 @@
 <template>
-    <div>
+    <div id="app">
         <h1>Currency Converter</h1>
-        <input v-model="amount" type="number" placeholder="Enter an amount">
-        <select v-model="fromCurrency">
-            <option v-for="currency in currencies" :key="currency">{{ currency }}</option>
-        </select>
-        <select v-model="toCurrency">
-            <option v-for="currency in currencies" :key="currency">{{ currency }}</option>
-        </select>
-        <button @click="convert">Convert</button>
-        <div v-if="result">
-            <p>Result: {{ result }}</p>
+        <div class="container">
+            <div class="container-one">
+                <select name="first-currency" id="first-currency">
+                    <option value=”AED“>AED></option>
+                    <option value=”ARS”>ARS></option>
+                    <option value=”AUD”>AUD></option>
+                    <option value=”BGN”>BGN></option>
+                    <option value=”BRL”>BRL></option>
+                    <option value=”CAD”>CAD></option>
+                    <option value=”CHF”>CHF></option>
+                    <option value=”CNY”>CNY></option>
+                    <option value=”DKK”>DKK></option>
+                    <option value=”DOP”>DOP></option>
+                    <option value=”EGP”>EGP></option>
+                    <option value=”EUR”>EUR></option>
+                    <option value=”GBP”>GBP></option>
+                    <option value=”HKD”>HKD></option>
+                    <option value=”ILS”>ILS></option>
+                    <option value=”INR”>INR></option>
+                    <option value=”JPY”>JPY></option>
+                    <option value=”LPB”>LPB></option>
+                    <option value=’MAD”>MAD></option>
+                    <option value=”NOK”>NOK></option>
+                    <option value=”NZD”>NZD></option>
+                    <option value=”RUB”>RUB></option>
+                    <option value=”SAR”>SAR></option>
+                    <option value=”SEK”>SEK></option>
+                    <option value=”SGD”>SGD></option>
+                    <option value=”THB”>THB></option>
+                    <option value=”TRY”>TRY></option>
+                    <option value=”USD”>USD></option>
+                    <option value=”UYU”>UYU></option>
+                    <option value=”ZAR”>ZAR></option>
+                    <option value=”ZWL”>ZWL></option>
+                </select>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
-
 export default {
-    data() {
-        return {
-           amount: 0,
-           fromCurrency: 'USD',
-           toCurrency: 'EUR',
-           result: null,
-           currencies: ['USD', 'EUR', 'GBP', 'JPY', 'CAD', 'AUD'],
-           apiKey: '', // Get your own API key from Exchange Rates API
-           apiUrl: 'https://api.exchangeratesapi.io/latest',
-        };
-    },
-    methods() {
-        
-    }
+    
 }
 </script>
